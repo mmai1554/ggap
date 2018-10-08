88ac18e9ec6ea8c106501788899a7924a66c2fdf
Michael Mai <mm@mai-internet.de>
2018-10-08 06:52:12 +0200

    fix line-height, add position relative for header when mobile

--- js/mi.js
+++ js/mi.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function ($) {
-    var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-    if (isFirefox) {
-        $('#HomeStatic').addClass('mi-show');
-        $('#HomeVideo').hide('mi-hide');
-    }
-});
+// jQuery(document).ready(function ($) {
+//     var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+//     if (isFirefox) {
+//         $('#HomeStatic').addClass('mi-show');
+//         $('#HomeVideo').hide('mi-hide');
+//     }
+// });

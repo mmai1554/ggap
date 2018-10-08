6828b6e0802f08366b80b9a4243b7d694b34f805
Michael Mai <mm@mai-internet.de>
2018-10-08 06:36:06 +0200

    fix line-height, add position relative for header when mobile

--- js/mi.js
+++ js/mi.js
@@ -1,9 +1,7 @@
-jQuery(document).ready(function($) {
-    // $(".link_tarifrechner").on("click", function(e){
-    //     var $confirm1 = alert('Hinweis: Die Berechnung über einen Tarifrechner kann immer nur einen Richtwert darstellen.'+"\n"+
-    //         'Für ein exaktes Angebot über Leistung sowie Prämie kontaktieren Sie uns bitte, da mit diversen Gesellschaften Rahmenverträge bestehen, '+
-    //         'die nicht mithilfe eines Tarifrechners abgedeckt werden können.'
-    //     );
-    //     $confirm1;
-    // });
+jQuery(document).ready(function ($) {
+    var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+    if (isFirefox) {
+        $('#HomeStatic').addClass('mi-show');
+        $('#HomeVideo').hide('mi-hide');
+    }
 });
